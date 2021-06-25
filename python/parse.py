dd93274805cfe60bdfd72a4f3363c7b22c59b6d6
thill <thill@tntech.edu>
2021-06-24 22:27:49 -0500

    added screenshots to show launch_06_24_2021

--- python/parse.py
+++ python/parse.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
+# Data Acquisition Rocket - GSET - Tennessee Technological University
+# Tristan Hill - June 24, 2021
+
+#import some libraries
 import csv
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 
-path = "../launch-data/"
+#set the local path to the data files
+path = "launch-data/"
 
+#open the file 
 with open(path+'NANO-1-06-24-2021.txt', newline='') as csvfile:
+	# create file identifiers for managing the input and output files
 	fid_in = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	#fid_out = open(path+'NANO-1-06-24-2021-parsed.txt','w') #not using yet
 
-	fid_out = open(path+'NANO-1-06-24-2021-parsed.txt','w')
-
+	# initialize empty lists to append the data to
 	orientation_x = []
 	orientation_y = []
 	orientation_z = []
@@ -25,53 +30,57 @@ with open(path+'NANO-1-06-24-2021.txt', newline='') as csvfile:
 	linearacceleration_y=[]
 	linearacceleration_z=[]
 
-
+	# iterate through the rows of data
 	for i,row in enumerate(fid_in):
-
+		#do stuff only if the row is not empty
 		if len(row)>0:
-			items=str(row[0]).split(":")
+			# split the names from the values delimited by ":"
+			items=str(row[0]).split(":") 
 			
 			if items[0]=="Orientation":
+				# split the three values delimited by ","
 				values=items[1].split(",")
+				# append the values as floats the the lists we initialized
 				orientation_x.append(float(values[0]))
 				orientation_y.append(float(values[1]))
-				orientation_z.append(float(values[2]))
-
-			elif items[0]=="RotationVector":
+				orientation_z.append(float(values[2]))	
+				
+			elif items[0]=="RotationVector":  # repeat for the other sensor types
 				values=items[1].split(",")
 				rotationvector_x.append(float(values[0]))
 				rotationvector_y.append(float(values[1]))
 				rotationvector_z.append(float(values[2]))
 
-			elif items[0]=="LinearAcceleration":
+			elif items[0]=="LinearAcceleration": 
 				values=items[1].split(",")
 				linearacceleration_x.append(float(values[0]))
 				linearacceleration_y.append(float(values[1]))
 				linearacceleration_z.append(float(values[2]))
 
-	fid_out.close()
+	# fid_out.close() # coming soon
+	N=300 # choose an index to stop at 
+	tstart=2150
+	tstop=tstart+N
+	t=np.arange(tstart,tstop) # create a range of values with numpy
 
-	N=2500
-	t=np.arange(0,N)
+	plt.figure(1) # setup a figure 1 window
 
-	plt.figure(1)
-
-	plt.subplot(311)
-	plt.plot(t,orientation_x[0:N])
-	plt.plot(t,orientation_y[0:N])
-	plt.plot(t,orientation_z[0:N])
+	plt.subplot(311) # setup a subplot (#rows #cols currentindex)
+	plt.plot(t,orientation_x[tstart:tstop])
+	plt.plot(t,orientation_y[tstart:tstop])
+	plt.plot(t,orientation_z[tstart:tstop])
 	plt.ylabel('Orientation')
 
 	plt.subplot(312)
-	plt.plot(t,rotationvector_x[0:N])
-	plt.plot(t,rotationvector_y[0:N])
-	plt.plot(t,rotationvector_z[0:N])
+	plt.plot(t,rotationvector_x[tstart:tstop])
+	plt.plot(t,rotationvector_y[tstart:tstop])
+	plt.plot(t,rotationvector_z[tstart:tstop])
 	plt.ylabel('RotationVector')
 
 	plt.subplot(313)
-	plt.plot(t,linearacceleration_x[0:N])
-	plt.plot(t,linearacceleration_y[0:N])
-	plt.plot(t,linearacceleration_z[0:N])
+	plt.plot(t,linearacceleration_x[tstart:tstop])
+	plt.plot(t,linearacceleration_y[tstart:tstop])
+	plt.plot(t,linearacceleration_z[tstart:tstop])
 	plt.ylabel('LinearAcceleration')
 
-	plt.show()
+	plt.show() # show the figure
